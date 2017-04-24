716239fecd67997433708191a859806f6567ccb5
nguyen280795 <nguyenpham.280795@gmail.com>
2017-04-24 20:53:06 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    //Hover menu
+    $(".menu ul li p:eq(0)").addClass("hover-menu");
+
+    $(".menu ul li p").click(function () {
+        $(".menu ul li p").removeClass("hover-menu");
+        $(this).addClass("hover-menu");
+    });
+});
